3986390f45b3443e73ea91a926498389cb065ed4
marcopisani002 <pisanimarco34@gmail.com>
2022-11-02 15:29:08 +0100

    add graphics

--- js/dom.js
+++ js/dom.js
@@ -1,42 +1,46 @@
-const numQuadrati= 10 * 10;
-const boardContainerEl =document.querySelector("board-container");
+const numQuadrati = 10 * 10;
+const boardContainerEl = document.querySelector(".board-container");
 
 
 
 
 
 
-for (let i = 0; i < numQuadrati; i++) {
-    
+for (let i = 1; i <= numQuadrati; i++) {
 
-   //boardContainerEl.innerHTML += `<div class="board-square " > ${i} </div>`;
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-   
-     // let color="";
-    
-    if (i % 3 === 0) {
-        //color="bg-warning";
-        console.log(`${i} FIZZ`);
 
-    } 
-    if (i % 5 === 0) {
+    boardContainerEl.innerHTML += `<div class="board-square " > ${i} </div>`;
 
-        console.log(`${i} BUZZ`);
 
-    } 
-     if ((i % 5 === 0) && (i % 3 === 0)) {
+
+
+
+
+
+
+
+
+
+     let color="";
+    if (i % 5 === 0 && i % 3 === 0) {
 
         console.log(`${i} CIAOOOOO`);
 
+    }
+    else if (i % 3 === 0) {
+        color="bg-warning";
+        console.log(`${i} FIZZ`);
+
+    }
+
+    else if (i % 5 === 0) {
+
+        console.log(`${i} BUZZ`);
+
+    }
+
+    else {
+        console.log(i);
     }
 
 }
