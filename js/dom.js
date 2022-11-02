ff7329b439aa7cc89f9d2e4a33340d0ce01d9457
marcopisani002 <pisanimarco34@gmail.com>
2022-11-02 16:18:12 +0100

    end coding

--- js/dom.js
+++ js/dom.js
@@ -1,6 +1,6 @@
 const numQuadrati = 10 * 10;
 const boardContainerEl = document.querySelector(".board-container");
-const squareInput=document.querySelector(".square-input")
+const squareInput = document.querySelector(".square-input")
 
 
 
@@ -8,46 +8,36 @@ const squareInput=document.querySelector(".square-input")
 
 for (let i = 1; i <= numQuadrati; i++) {
 
-
-    boardContainerEl.innerHTML += `<div class="board-square " > ${i} </div>`;
-
-
-
-
-
-
-
-
-
-
-
-     let color="";
     if (i % 5 === 0 && i % 3 === 0) {
-        color="bg-dark";
-        //const ciaoColorEl = document.querySelector(".fizzbuzz-square");
+
         //console.log(`${i} CIAOOOOO`);
-        boardContainerEl.innerHTML += `<div class="board-square " > 
-        <div class="square-input " > CIAOOOOO </div>
+        boardContainerEl.innerHTML += `<div class="board-square fizzbuzz-square " > 
+        <div class="square-input " > FIZZBUZZ</div>
          </div>`;
     }
     else if (i % 3 === 0) {
-        color="bg-dark";
+
         //console.log(`${i} FIZZ`);
-        boardContainerEl.innerHTML += `<div class="board-square " > 
+
+        boardContainerEl.innerHTML += `<div class="board-square fizz-square  " > 
         <div class="square-input " > FIZZ </div>
          </div>`;
     }
 
     else if (i % 5 === 0) {
 
-       // console.log(`${i} BUZZ`);
-        boardContainerEl.innerHTML += `<div class="board-square " > 
+        // console.log(`${i} BUZZ`);
+
+        boardContainerEl.innerHTML += `<div class="board-square buzz-square " > 
         <div class="square-input " >BUZZ </div>
          </div>`;
     }
 
     else {
-        console.log(i);
+        // console.log(i);
+
+        boardContainerEl.innerHTML += `<div class="board-square square-input  no-square" > ${i} </div>`;
+
     }
 
 }
