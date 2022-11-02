f059cb9ac2b4a4ffa3735123942714441735f9e7
marcopisani002 <pisanimarco34@gmail.com>
2022-11-02 15:56:51 +0100

    add styles

--- js/dom.js
+++ js/dom.js
@@ -1,6 +1,6 @@
 const numQuadrati = 10 * 10;
 const boardContainerEl = document.querySelector(".board-container");
-
+const squareInput=document.querySelector(".square-input")
 
 
 
@@ -23,20 +23,27 @@ for (let i = 1; i <= numQuadrati; i++) {
 
      let color="";
     if (i % 5 === 0 && i % 3 === 0) {
-
-        console.log(`${i} CIAOOOOO`);
-
+        color="bg-dark";
+        //const ciaoColorEl = document.querySelector(".fizzbuzz-square");
+        //console.log(`${i} CIAOOOOO`);
+        boardContainerEl.innerHTML += `<div class="board-square " > 
+        <div class="square-input " > CIAOOOOO </div>
+         </div>`;
     }
     else if (i % 3 === 0) {
-        color="bg-warning";
-        console.log(`${i} FIZZ`);
-
+        color="bg-dark";
+        //console.log(`${i} FIZZ`);
+        boardContainerEl.innerHTML += `<div class="board-square " > 
+        <div class="square-input " > FIZZ </div>
+         </div>`;
     }
 
     else if (i % 5 === 0) {
 
-        console.log(`${i} BUZZ`);
-
+       // console.log(`${i} BUZZ`);
+        boardContainerEl.innerHTML += `<div class="board-square " > 
+        <div class="square-input " >BUZZ </div>
+         </div>`;
     }
 
     else {
